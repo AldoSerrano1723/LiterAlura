a9525337e8db64624afd2809705a166fe2c4fe1f
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-12-11 08:18:04 -0600

    Feat: Añade metodo top10Libros

--- src/main/java/com/aldocurso/LiterAlura/principal/Principal.java
+++ src/main/java/com/aldocurso/LiterAlura/principal/Principal.java
@@ -8,6 +8,7 @@ import com.aldocurso.LiterAlura.service.ConvertirDatos;
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -37,36 +38,50 @@ public class Principal {
 
             switch (opcion){
                 case 1:
-                    System.out.println("\nINGRESE EL TITULO QUE DESA BUSCAR:");
-                    String titulo = sc.nextLine();
-                    String tituloCodificado = URLEncoder.encode(titulo, StandardCharsets.UTF_8);
-                    String urlBusquedaTitulo = URL_BASE + "?search=" + tituloCodificado;
-                    RespuestaAPI respuestaAPI = convertirDatos.obtenerDatos(consumoApi.obtenerDatos(urlBusquedaTitulo), RespuestaAPI.class);
-                    Optional <DatosLibro> libroBuscado = respuestaAPI.listaDeLibros().stream()
-                                    .findFirst();
-                    if (libroBuscado.isPresent()){
-                        System.out.println("\n--- LIBRO ECONTRADO ---");
-                        System.out.println("LOS DATOS DEL LIBRO SON: " + libroBuscado.get());
-                        System.out.println("\n");
-                    }else {
-                        System.out.println("\n***** NO ESTA EL LIBRO *****");
-                        System.out.println("\n");
-                    }
+                    buscarTitulo();
                     break;
                 case 2:
-                    System.out.println("2");
+                    top10Libros();
                     break;
                 case 3:
                     System.out.println("3");
                     break;
                 case 4:
-                    System.out.println("ADIOS");
+                    System.out.println("-- ADIOS --");
                     break;
                 default:
-                    System.out.println("OPCION NO VALIDA ");
+                    System.out.println("*** OPCION NO VALIDA ***");
                     break;
             }
         }
         System.out.println("FIN DEL PROGRAMA");
+    }
+
+    public void buscarTitulo(){
+        System.out.println("\nINGRESE EL TITULO QUE DESA BUSCAR:");
+        String titulo = sc.nextLine();
+        String tituloCodificado = URLEncoder.encode(titulo, StandardCharsets.UTF_8);
+        String urlBusquedaTitulo = URL_BASE + "?search=" + tituloCodificado;
+        RespuestaAPI respuestaAPI = convertirDatos.obtenerDatos(consumoApi.obtenerDatos(urlBusquedaTitulo), RespuestaAPI.class);
+        Optional <DatosLibro> libroBuscado = respuestaAPI.listaDeLibros().stream()
+                .findFirst();
+        if (libroBuscado.isPresent()){
+            System.out.println("\n--- LIBRO ECONTRADO ---");
+            System.out.println("LOS DATOS DEL LIBRO SON: " + libroBuscado.get());
+            System.out.println("\n");
+        }else {
+            System.out.println("\n***** NO ESTA EL LIBRO *****");
+            System.out.println("\n");
+        }
+    }
+
+    public void top10Libros(){
+        System.out.println("\n--- TOP 10 LIBROS MAS DESCARGADOS ---");
+        RespuestaAPI respuestaAPI = convertirDatos.obtenerDatos(consumoApi.obtenerDatos(URL_BASE), RespuestaAPI.class);
+        respuestaAPI.listaDeLibros().stream()
+                .sorted(Comparator.comparing(DatosLibro::numeroDeDescargas).reversed())
+                .limit(10)
+                .forEachOrdered(System.out::println);
+        System.out.println("\n");
     }
 }
